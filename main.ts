fceea85d9f7c07358de06fe16a5f6ecb5d5d135d
jmichalenko <50920207+jmichalenko@users.noreply.github.com>
2023-04-27 14:56:51 -0400

    Used Wukong Library

--- main.ts
+++ main.ts
@@ -1,36 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 100)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
-        basic.showIcon(IconNames.Heart)
-        basic.pause(50)
+        wuKong.setAllMotor(100, 100)
+        basic.showNumber(1)
+    } else if (receivedNumber == 2) {
+        wuKong.setAllMotor(-100, -100)
+        basic.showNumber(2)
+    } else if (receivedNumber == 3) {
+        wuKong.setAllMotor(0, 0)
+        basic.showNumber(3)
     } else {
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-    }
-    if (receivedNumber == 2) {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 100)
-        basic.pause(50)
-    } else {
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-    }
-    if (receivedNumber == 3) {
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, 100)
-        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, 100)
-        basic.pause(50)
-    } else {
-        if (receivedNumber == 4) {
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, 100)
-            kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, 100)
-            basic.pause(50)
-        } else {
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-            kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
-        }
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
-        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+        wuKong.setAllMotor(0, 0)
     }
 })
 radio.setGroup(1)
